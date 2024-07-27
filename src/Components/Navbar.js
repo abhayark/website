828f26326840ce4225c2c99f39553cdc83c0a830
abhayark <pcabhay0@gmail.com>
2024-07-27 21:06:29 +0530

    First build

---
+++ src/Components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Navbar() {
+  return (
+    <Container>
+        <header>
+
+
+        </header>
+    </Container>
+  )
+}
+
+export default Navbar
+
+const Container = styled.div`
+        height: 60px;
+        background-color:black;
+        
+`
