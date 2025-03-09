642721d172b0d554cfba4fbe70de0fe848ad78df
abhayark <pcabhay0@gmail.com>
2025-03-09 22:57:26 +0530

    Starting a cart overhaul

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -12,56 +12,69 @@ const Cart = ({ cart, onRemove }) => {
   );
 
   return (
-    <div>
+    <>
       <Navbar cartCount={cart.length} />
       <div className="cart-container">
         <h2 className="title"> Your Cart 🛒</h2>
         {cart.length === 0 ? (
           <p className="empty-cart">Your cart is empty</p>
         ) : (
-          <ul className="cart-list">
-            {cart.map((product, index) => (
-              <li
-                key={product._id}
-                className="cart-item"
-                onClick={() => {
-                  goto(`/products/${product._id}`);
-                  console.log("clicked!");
-                }}
-              >
-                <img
-                  src={product.img}
-                  alt={product.product_name}
-                  className="cart-img"
-                />
-                <div className="cart-info">
-                  <h3 className="cart-product-name">{product.product_name}</h3>
-                  <p className="cart-product-price">
-                    Price: ₹{Number(product.price).toLocaleString("en-IN")}
-                  </p>
-                  <button
-                    className="remove-btn"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      onRemove(index);
-                      console.log("clicked!");
-                    }}
-                  >
-                    Remove
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul>
+          <div>
+            <ul className="cart-list">
+              {cart.map((product, index) => (
+                <li
+                  key={product._id}
+                  className="cart-item"
+                  onClick={() => {
+                    goto(`/products/${product._id}`);
+                    console.log("clicked!");
+                  }}
+                >
+                  <img
+                    src={product.img}
+                    alt={product.product_name}
+                    className="cart-img"
+                  />
+                  <div className="cart-info">
+                    <h3 className="cart-product-name">
+                      {product.product_name}
+                    </h3>
+                    <h3 className="cart-product-seller">{product.seller}</h3>
+
+                    <p className="cart-product-price">
+                      Price: ₹{Number(product.price).toLocaleString("en-IN")}
+                    </p>
+                    <button
+                      className="remove-btn"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        onRemove(index);
+                        console.log("clicked!");
+                      }}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         )}
-        <p className="cart-product-price">
-          <strong>Total: ₹{Number(totalPrice).toLocaleString("en-IN")}</strong>
-        </p>
-        <button className="continue-shopping-btn" onClick={() => goto("/")}>
-          Continue Shopping
-        </button>
+        <div>
+          <p className="cart-product-price">
+            <strong>
+              Total: ₹{Number(totalPrice).toLocaleString("en-IN")}
+            </strong>
+          </p>
+          <button className="continue-shopping-btn" onClick={() => goto("/")}>
+            Buy
+          </button>
+          <button className="continue-shopping-btn" onClick={() => goto("/")}>
+            Continue Shopping
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
